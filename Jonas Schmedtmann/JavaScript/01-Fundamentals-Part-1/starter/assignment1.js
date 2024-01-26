0a4c9b1ebcd3b8460a1a285d2839c528875a96d6
Joe-Townsend <joetown30@outlook.com>
2024-01-26 13:16:56 +0000

    Fundamentals Part 1 update

---
+++ Jonas Schmedtmann/JavaScript/01-Fundamentals-Part-1/starter/assignment1.js
@@ -0,0 +1,13 @@
+const country = 'England'
+const continent = 'Europe'
+let population = 56000000
+
+// console.log("Country: " + country+ "\nContinent: " + continent + "\nPopulation: " + population);
+
+const isIsland = true;
+let language = 'English';
+
+// console.log(typeof isIsland)
+// console.log(typeof population)
+// console.log(typeof country)
+// console.log(typeof language)
--- Jonas Schmedtmann/JavaScript/01-Fundamentals-Part-1/starter/script.js
+++ Jonas Schmedtmann/JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,12 @@
+/* Data Types: 
+Number- Floating point numbers. used for decimals and integers
+String- Sequence of Characters, used for text
+Boolean- Logical type that can only be true or false, used for taking decisions
+Undefined- Value taken by a variuable that is not yet defined
+Null- Also means 'empty value'
+Symbol (ES2015)- Value that is unique and cannot be changed [not useful]
+BigInt (ES2020)- Larger integers than the number type can hold
+
 let js = 'amazing';
 // if (js === 'amazing') alert('JavaScript is FUN!');
 
@@ -19,10 +28,38 @@ let myCurrentJob = 'Supervisor';
 console.log(myFirstJob);
 console.log(myCurrentJob);
 console.log(PI);
-console.log("First Job: " + myFirstJob + ", Current Job: " + myCurrentJob);
+console.log("First Job: " + myFirstJob + ", Current Job: " + myCurrentJob);*/
 
-let country = 'England'
-let continent = 'Europe'
-let population = 56000000
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
 
-console.log("Country: " + country + "\nContinent: " + continent + "\nPopulation: " + population)
+// console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Joe');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+let age = 30;
+console.log(age);
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+// birthYear = 1990;
+
+var job = ';programmer';
+job = 'teacher';
+
+lastName= 'Townsend'
+console.log(lastName)
