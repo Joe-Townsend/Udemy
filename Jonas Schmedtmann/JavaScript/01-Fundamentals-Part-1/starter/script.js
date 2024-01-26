9bb28c300438b4834b3f91429a41927056d7e1d7
Joe-Townsend <joetown30@outlook.com>
2024-01-26 12:24:17 +0000

    Fundamentals Part1 changes

--- Jonas Schmedtmann/JavaScript/01-Fundamentals-Part-1/starter/script.js
+++ Jonas Schmedtmann/JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -7,5 +7,16 @@ console.log("23");
 
 let firstName = "Joe";
 console.log(firstName);
-console.log(firstName);
-console.log(firstName);
+
+let joe_josh = "JJ";
+console.log(joe_josh);
+
+let PI = 3.1415;
+
+let myFirstJob = 'IT Support';
+let myCurrentJob = 'Supervisor';
+
+console.log(myFirstJob);
+console.log(myCurrentJob);
+console.log(PI);
+console.log("First Job: " + myFirstJob + ", Current Job: " + myCurrentJob);
