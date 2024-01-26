b61bb50adbae00b301a27d16ef26bcd5be0048ea
Joe-Townsend <joetown30@outlook.com>
2024-01-26 12:11:00 +0000

    Update

--- Jonas Schmedtmann/JavaScript/01-Fundamentals-Part-1/starter/script.js
+++ Jonas Schmedtmann/JavaScript/01-Fundamentals-Part-1/starter/script.js
@@ -1,4 +1,11 @@
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+// if (js === 'amazing') alert('JavaScript is FUN!');
 
 console.log(40+8+23-10);
+console.log("Joe");
+console.log("23");
+
+let firstName = "Joe";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
