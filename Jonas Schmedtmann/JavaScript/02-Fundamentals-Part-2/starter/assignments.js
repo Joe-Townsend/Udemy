74aa8546b388d7f2401c5f3090a320e1740cf598
Joe-Townsend <joetown30@outlook.com>
2024-01-30 15:04:44 +0000

    Fundamentals Part 2 Update 1

--- Jonas Schmedtmann/JavaScript/02-Fundamentals-Part-2/starter/assignments.js
+++ Jonas Schmedtmann/JavaScript/02-Fundamentals-Part-2/starter/assignments.js
@@ -1,1 +1,9 @@
 // ASSIGNMENTS
+
+function describeCountry(country, population, capitalCity) {
+    console.log(`${country} has ${population} million people and its capital city is ${capitalCity}`);
+}
+
+describeCountry('England', 56, 'London');
+describeCountry('Canada', 28, 'Ottawa');
+describeCountry('Australia', 32, 'Canberra');
